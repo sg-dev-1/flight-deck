b94a427e53fafbec609386f0100dc862b058e7e6
izyo <skillgalaxy@pm.me>
2025-04-20 19:12:30 +0300

    add db context

---
+++ flight-deck/FlightDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class FlightDbContext : DbContext
+{
+    public FlightDbContext(DbContextOptions<FlightDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Flight> Flights { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Flight>()
+            .HasIndex(f => f.FlightNumber)
+            .IsUnique();
+    }
+}
