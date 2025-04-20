f0f80c19049c863bc5397ed33435728b2e7e3d65
izyo <skillgalaxy@pm.me>
2025-04-20 18:58:54 +0300

    add flight base model

---
+++ flight-deck/Models/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Flight
+{
+    // Primary Key
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(10)]
+    public string FlightNumber { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100)]
+    public string Destination { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime DepartureTime { get; set; }
+
+    [Required]
+    [StringLength(10)]
+    public string Gate { get; set; } = string.Empty;
+}
