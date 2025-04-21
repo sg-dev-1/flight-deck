4b5465b9380feb69229ab09fd6712040178934b2
sg-dev-1 <izyo@pm.me>
2025-04-21 07:58:38 +0300

    Revert "remove status to client"

--- flight-deck/Services/IFlightRepository.cs
+++ flight-deck/Services/IFlightRepository.cs
@@ -4,7 +4,7 @@ namespace FlightDeck.Services
 {
     public interface IFlightRepository
     {
-        Task<IEnumerable<Flight>> GetAllFlightsAsync(string? destination);
+        Task<IEnumerable<Flight>> GetAllFlightsAsync(string? destination, string? status);
         Task<Flight?> GetFlightByIdAsync(Guid id);
         Task<Flight?> GetFlightByNumberAsync(string flightNumber);
         Task<Flight> AddFlightAsync(Flight flight);
